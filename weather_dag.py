924e86e150e3a6c7ba5a70cc425e5615a2dc9ff5
troyegilbert <troye.gilbert@azubiafrica.org>
2023-08-30 23:10:32 +0300

    created DAG

---
+++ weather_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
+
+# Convert Kelvin to Fahrenheit
+def kelvin_to_fahrenheit(temp_in_kelvin):
+    temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
+    return temp_in_fahrenheit
+
+# Data Transformation
+def transform_load_data(task_instance):
+    data = task_instance.xcom_pull(task_ids="extract_weather_data")
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    time_of_record = datetime.utcfromtimestamp(data['dt'] + data['timezone'])
+    sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
